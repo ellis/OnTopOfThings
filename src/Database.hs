2ca02b4e09cb423cafd44f90d5529adae7e32aed
Ellis Whitehead <ellis@ellisw.net>
2013-12-28 19:49:07 +0100

    about to try out esqueleto

--- src/Database.hs
+++ src/Database.hs
@@ -18,7 +18,8 @@ import Control.Monad.Logger (NoLoggingT)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Control.Monad.Trans.Resource (ResourceT)
 import Database.Persist
-import Database.Persist.Sqlite
+import Database.Persist.Sqlite as S
+import qualified Database.Esqueleto
 import Database.Persist.TH
 
 import Data.Aeson (encode, decode)
@@ -50,8 +51,9 @@ databaseUpdateIndexes = do
   -- TODO: There's a better way to do this, I'm sure -- something with joins or something.
   -- Get a list of pending tasks
   entities1 <- selectList [PropertyTable ==. "item", PropertyName ==. "type", PropertyValue ==. "task"] []
-  mapM_ choose entities1
-  selectList [PropertyTable ==. "item", PropertyName ==. "type", PropertyValue ==. "task"] []
+  return ()
+  --mapM_ choose entities1
+  --selectList [PropertyTable ==. "item", PropertyName ==. "type", PropertyValue ==. "task"] []
 
 --databaseProcessCommandTable :: SqlPersistT (NoLoggingT (ResourceT IO)) ()
 --databaseProcessCommandTable = do
