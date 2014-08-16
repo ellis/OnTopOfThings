34c033cf4bcf5501caa689d2cc52638603f8cbe5
Ellis Whitehead <ellis@server.fake>
2014-08-16 08:33:42 +0200

    progress with creating items from patches

--- couchdb/client.js
+++ couchdb/client.js
@@ -5,59 +5,59 @@ var http = require('http'),
 var idSeen_l = {};
 var item_l = [];
 
-var stream = JSONStream.parse(['results', true, 'id']) //rows, ANYTHING, doc
-stream.on('data', function(data) {
-	var l = data.split("|");
-	if (l.length == 3) {
-		var id = l[0];
-		if (!(id in idSeen_l)) {
-			idSeen_l[id] = true;
-			console.log("ID: "+id);
+function loadFromView(url) {
+	var stream2 = JSONStream.parse(['rows', true, 'value', 'data']);
+	stream2.on('data', function(data) {
+		var id = data.id;
+		data['_id'] = id;
+		delete data['id'];
+		console.log("ITEM: "+JSON.stringify(data));
 
-			var stream2 = JSONStream.parse(['rows', true, 'value', 'data']);
-			stream2.on('data', function(data) {
-				data['_id'] = id;
-				delete data['id'];
-				console.log("ITEM: "+JSON.stringify(data));
+		request.put(
+			{
+				url: 'http://127.0.0.1:5984/otot/'+id,
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				json: data
+			},
+			function (e, r, body) {
+				console.log("BODY: "+JSON.stringify(body));
+			}
+		);
+	});
+	request({url: url})
+		.pipe(stream2);
+}
 
-				var options3 = {
-					host: 'http://127.0.0.1',
-					port: 5984,
-					path: '/otot/' + id,
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				};
-				request.put(
-					{
-						url: 'http://127.0.0.1:5984/otot/'+id,
-						headers: {
-							'Content-Type': 'application/json'
-						},
-						json: data
-					},
-					function (e, r, body) {
-						console.log("BODY: "+JSON.stringify(body));
-					}
-				);
-			});
-			request({url: 'http://127.0.0.1:5984/otot/_design/main/_view/items?startkey=["'+id+'"]&endkey=["'+id+'",{}]'})
-				.pipe(stream2);
+function loadFromChanges(since) {
+	var stream = JSONStream.parse(['results', true, 'id']) //rows, ANYTHING, doc
+	stream.on('data', function(data) {
+		var l = data.split("|");
+		if (l.length == 3) {
+			var id = l[0];
+			if (!(id in idSeen_l)) {
+				idSeen_l[id] = true;
+				console.log("ID: "+id);
+				loadFromView('http://127.0.0.1:5984/otot/_design/main/_view/items?startkey=["'+id+'"]&endkey=["'+id+'",{}]');
+			}
 		}
-	}
-});
-stream.on('root', function(root, count) {
-  if (!count) {
-    console.log('no matches found:', root);
-  }
-});
-/*stream.on('end', function() {
-	console.log("ALL ITEMS:");
-	for (var i = 0; i < item_l.length; i++) {
-		console.log(JSON.stringify(item_l[i]));
-	}
-});*/
+	});
+	stream.on('root', function(root, count) {
+	  if (!count) {
+		console.log('no matches found:', root);
+	  }
+	});
+	/*stream.on('end', function() {
+		console.log("ALL ITEMS:");
+		for (var i = 0; i < item_l.length; i++) {
+			console.log(JSON.stringify(item_l[i]));
+		}
+	});*/
 
-request({url: 'http://127.0.0.1:5984/otot/_changes?since=1039'})
-  .pipe(stream);
+	request({url: 'http://127.0.0.1:5984/otot/_changes?since='+since})
+	  .pipe(stream);
+}
+
+loadFromView('http://127.0.0.1:5984/otot/_design/main/_view/items?group_level=1');
+//loadFromChanges(1099);
--- couchdb/couchdb_notes.md
+++ couchdb/couchdb_notes.md
@@ -3,11 +3,20 @@
 - [x] Use ``curl -X GET http://127.0.0.1:5984/otot/_changes\?feed\=continuous\&since\=978\&style\=all_docs`` to get a list of recent patches
 - [x] the ID for a patch should be: itemId|utc|randomNumber
 - [x] Parse the patch id and request a list of the item IDs from the appropriate view
-- [ ] client.js: generates bulk update pack for creating documents with the itemIds. (The view will need to include revision number if a document already exists)
-- [ ] _view/items: need to output the rev if present
-- [ ] Create a script to run all of the above
+- [x] client.js: generates update request for creating documents with the itemIds. (The view will need to include revision number if a document already exists)
+- [x] _view/items: need to output the rev if present
+- [x] Create a script to run all of the above
+- [ ] couchdb: Rename 'item' view to 'patch' view
+- [ ] couchdb: Create views based on item documents
+- [ ] website: adapt for new views
+- [ ] website: view by horizon
+- [ ] website: view of projects
+- [ ] website: view by project
+- [ ] website: let user mark items as done
+- [ ] website: let user mark change item's horizon
+- [ ] client.js: move aggregation of snapshots and patches from couchdb view to client.js
+- [ ] modularize client.js to create a script that updates everything and one that updates just based on _changes
 - [ ] Create a program that subscribes to the changelog and updates the item documents when patches come in
-- [ ] Create views based on item documents
 
 # Notes
 
