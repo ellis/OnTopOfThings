1ea2aab771c0029e4168bd92e815f85835359bf8
Ellis Whitehead <ellis@ellisw.net>
2014-10-17 21:53:18 +0200

    server lists json files in data/

--- website2/server.js
+++ website2/server.js
@@ -1,5 +1,9 @@
+var fs = require('fs');
 var http = require('http');
+var jf = require('jsonfile');
+var path = require('path');
 var url = require('url');
+var util = require('util');
 
 var port = process.argv[2];
 
@@ -7,9 +11,17 @@ var server = http.createServer(function(request, response) {
 	response.writeHead(200, { 'content-type': 'application/json' });
 
 	var urlData = url.parse(request.url, true);
-	var date = new Date(urlData.query.iso);
+	//var date = new Date(urlData.query.iso);
+	
+	var filename_l = fs.readdirSync("./data/");
+	for (var i in filename_l) {
+		var filename = filename_l[i];
+		if (path.extname(filename) === ".json") {
+			response.write(filename);
+		}
+	}
 
-	if (urlData.pathname == "/api/parsetime") {
+	/*if (urlData.pathname == "/api/parsetime") {
 		var data = { hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds() };
 		response.write(JSON.stringify(data));
 	}
@@ -17,6 +29,7 @@ var server = http.createServer(function(request, response) {
 		var data = { unixtime: date.getTime() };
 		response.write(JSON.stringify(data));
 	}
+	*/
 	response.end();
 });
 
