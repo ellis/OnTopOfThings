6a800135d98a676a4a71cd3c9ce92b7a3ebe79dc
Ellis Whitehead <ellis@ellisw.net>
2014-10-18 10:05:53 +0200

    Reads in snapshots and patches, prints all items at /items

--- website2/server.js
+++ website2/server.js
@@ -4,64 +4,53 @@ var jf = require('jsonfile');
 var path = require('path');
 var url = require('url');
 var util = require('util');
+var _ = require('underscore');
 
 var port = process.argv[2];
 
-var item_m = {};
+function getAllItems() {
+	// Load all json files into a map to a list of 
+	var filename_l = fs.readdirSync("./data/");
+	// The files should be named in order of processing,
+	// so sort the array so that we can directly update the item list
+	var filename_l = _.filter(filename_l, function(filename) { return path.extname(filename) === ".json" });
+	filename_l.sort();
+
+	var item_m = {};
+	_.each(filename_l, function(filename) {
+		var contents = JSON.parse(fs.readFileSync("./data/"+filename, "utf8"));
+		if (contents.type === "snapshot") {
+			_.each(contents.items, function(item) {
+				item_m[item.id] = item;
+			});
+		}
+		// Patch with multiple ids, each receiving the same diffs
+		else if (contents.type === "patch1") {
+			_.each(contents.ids, function(id) {
+				_.each(contents.diffs, function(diff) {
+					if (diff[0] === "=") {
+						var name = diff[1];
+						item_m[id][name] = diff[2];
+					}
+				});
+			});
+		}
+	});
+
+	return _.toArray(item_m);
+}
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, { 'content-type': 'application/json' });
 
 	var urlData = url.parse(request.url, true);
 	//var date = new Date(urlData.query.iso);
-	
-	// Load all json files into a map to a list of 
-	var filename_l = fs.readdirSync("./data/");
-	// The files should be named in order of processing,
-	// so sort the array so that we can directly update the item list
-	filename_l.sort();
-	for (var i in filename_l) {
-		var filename = filename_l[i];
-		if (path.extname(filename) === ".json") {
-			var contents = JSON.parse(fs.readFileSync("./data/"+filename, "utf8"));
-			if (contents.type === "snapshot") {
-				for (var j in contents.items) {
-					var item = contents.items[j];
-					item_m[item.id] = item;
-				}
-			}
-			// Patch with multiple ids, each receiving the same diffs
-			else if (contents.type === "patch1") {
-				for (var j in contents.ids) {
-					var id = contents.ids[j];
-					for (var k in contents.diffs) {
-						var diff = contents.diffs[k];
-						if (diff[0] === "=") {
-							var name = diff[1];
-							item_m[id][name] = diff[2];
-						}
-					}
-				}
-			}
-		}
+
+	if (urlData.pathname == "/items") {
+		var item_l = getAllItems();
+		response.write(JSON.stringify(item_l));
 	}
 
-	for (var i in item_m) {
-		var item = item_m[i];
-		response.write(item.id+": "+JSON.stringify(item)+"\n");
-	}
-
-	//response.write(filename+": "+JSON.stringify(file)+"\n");
-
-	/*if (urlData.pathname == "/api/parsetime") {
-		var data = { hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds() };
-		response.write(JSON.stringify(data));
-	}
-	else if (urlData.pathname == "/api/unixtime") {
-		var data = { unixtime: date.getTime() };
-		response.write(JSON.stringify(data));
-	}
-	*/
 	response.end();
 });
 
